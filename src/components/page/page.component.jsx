dce236ad58da14593d6547efa1fbb6a876645c54
Agris <agris.magdalenoks@gmail.com>
2021-08-21 14:42:52 +0300

    Update page.component.jsx

--- src/components/page/page.component.jsx
+++ src/components/page/page.component.jsx
@@ -7,9 +7,15 @@ function Page() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
+    const [selectCurrency, setSelectCurrency] = useState('');
+
+    console.log(selectCurrency)
+    const URL_KEY = `https://financialmodelingprep.com/api/v3/fx/EURUSD?apikey=8813ec2a91fb1d6a46c0de9f6bb67635`;
+
+    // ${handleChange.replace('/', '')}
 
     useEffect(() => {
-        fetch(API_KEY)
+        fetch(URL_KEY)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -23,6 +29,7 @@ function Page() {
             )
     }, [])
 
+
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -33,9 +40,16 @@ function Page() {
                 <h1>Forex live wall</h1>
                 <SelectCurrency items={items} />
                 {items.map(item => (
-                    <li key={item.ticker}>
-                        {item.ticker}{item.bid}{item.changes}
-                    </li>
+                    <tr>
+                        <td>{item.ticker}</td>
+                        <td>{item.bid}</td>
+                        <td>{item.ask}</td>
+                        <td>{item.open}</td>
+                        <td>{item.low}</td>
+                        <td>{item.high}</td>
+                        <td>{item.changes}</td>
+                        <td>{item.date}</td>
+                    </tr>
                 ))}
             </ul>
         );
